f5a253ddc601f1824001b6712b6be3a698543b00
MRdesignofc <maycongribeiro1@gmail.com>
2026-01-16 21:04:27 -0300

    Update guard.js

--- guard.js
+++ guard.js
@@ -1,12 +1,11 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.4/firebase-app.js";
+import { initializeApp, getApps } from "https://www.gstatic.com/firebasejs/10.12.4/firebase-app.js";
 import { getAuth, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.12.4/firebase-auth.js";
 
-const app = initializeApp(window.FIREBASE_CONFIG);
+const config = window.FIREBASE_CONFIG;
+const app = getApps().length ? getApps()[0] : initializeApp(config);
 const auth = getAuth(app);
 
-// BLOQUEIO DA HOME
+// Bloqueia home sem login
 onAuthStateChanged(auth, (user) => {
-  if (!user) {
-    window.location.replace("login.html");
-  }
+  if (!user) window.location.replace("login.html");
 });
